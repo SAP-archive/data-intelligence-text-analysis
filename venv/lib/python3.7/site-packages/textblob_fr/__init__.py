8dd5cc4491e2dd10d97a28de41c6980eb9fe14c3
Thorsten Hapke <53856509+thhapke@users.noreply.github.com>
2020-10-03 06:40:37 +0200

    init2

--- venv/lib/python3.7/site-packages/textblob_fr/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from textblob_fr.taggers import PatternTagger
-from textblob_fr.sentiments import PatternAnalyzer
-
-__version__ = '0.2.0'
-__author__ = 'Steven Loria'
-__license__ = "MIT"
